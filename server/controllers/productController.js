9aa66bbf63190dd27a5603c4f3d668d4a60d0787
fayiztv <tvfayiztv@gmail.com>
2025-06-27 17:31:00 +0530

    create product API added

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -8,3 +8,16 @@ export const getAllProducts = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+export const createProduct = async (req, res) => {
+  if (req.query.key !== process.env.ADMIN_KEY)
+    return res.status(401).send("Unauthorized");
+
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (err) {
+    res.status(400).json({ error: "Invalid data" });
+  }
+};
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -4,5 +4,6 @@ import { getAllProducts, createProduct } from "../controllers/productController.
 const router = express.Router();
 
 router.get("/", getAllProducts);
+router.post("/", createProduct);
 
 export default router;
