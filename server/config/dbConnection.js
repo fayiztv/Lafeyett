fe0eaef668182b991a3fbc7b152a73b43f03189a
fayiztv <tvfayiztv@gmail.com>
2025-06-27 17:42:42 +0530

    client added

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+    console.log("✅ Database connected");
   } catch (err) {
     console.error("❌ DB connection failed:", err);
     process.exit(1);
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./config/dbConnection.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 
@@ -11,7 +12,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-
+// Routes
+app.use("/api/products", productRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
