e6b9f193b1e4158b3fbfcd931cf8225e56509034
fayiztv <tvfayiztv@gmail.com>
2025-06-27 17:23:48 +0530

    database connected

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import { connectDB } from "./config/dbConnection.js";
 
 const app = express();
 
+// Connect DB
+connectDB();
 
 app.use(cors());
 app.use(express.json());
+
 
 
 
